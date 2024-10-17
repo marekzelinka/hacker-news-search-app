f0c5178f032568d23802177b35933b313cab0440
Marek Zelinka <mzelinka17@gmail.com>
2024-10-17 12:45:04 +0200

    covert to esm

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const path = require('path');
-const axios = require('axios');
+import axios from 'axios';
+import * as dateFns from 'date-fns';
+import express from 'express';
+import path from 'node:path';
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(import.meta.dirname, 'public')));
 
 app.set('view engine', 'pug');
 
-app.locals.dateFns = require('date-fns');
+app.locals.dateFns = dateFns;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.render('home', {
     title: 'Search Hacker News',
   });
@@ -20,18 +21,21 @@ async function searchHN(query) {
   const response = await axios.get(
     `https://hn.algolia.com/api/v1/search?query=${query}&tags=story&hitsPerPage=90`,
   );
+
   return response.data;
 }
 
 app.get('/search', async (req, res, next) => {
   try {
     const searchQuery = req.query.q;
+
     if (!searchQuery) {
       res.redirect(302, '/');
       return;
     }
 
     const results = await searchHN(searchQuery);
+
     res.render('search', {
       title: `Search results for: ${searchQuery}`,
       searchResults: results,
@@ -42,12 +46,14 @@ app.get('/search', async (req, res, next) => {
   }
 });
 
-app.use(function (err, req, res, next) {
-  console.error(err);
+app.use((error, _req, res, _next) => {
+  console.error(error);
+
   res.set('Content-Type', 'text/html');
   res.status(500).send('<h1>Internal Server Error</h1>');
 });
 
-const server = app.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
   console.log(`Hacker news server started on port: ${server.address().port}`);
 });
