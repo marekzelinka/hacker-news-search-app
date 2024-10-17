210936d622428be92ee25c8f0e6d49d6a5a9db37
Marek Zelinka <mzelinka17@gmail.com>
2024-10-17 12:40:01 +0200

    setup

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 
 async function searchHN(query) {
   const response = await axios.get(
-    `https://hn.algolia.com/api/v1/search?query=${query}&tags=story&hitsPerPage=90`
+    `https://hn.algolia.com/api/v1/search?query=${query}&tags=story&hitsPerPage=90`,
   );
   return response.data;
 }
